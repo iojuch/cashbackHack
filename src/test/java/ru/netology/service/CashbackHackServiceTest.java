baa9f758ad73fd0de4deab5ac9219991a9130dd1
Александр Забелинский <petrovich8384@mail.ru>
2023-10-02 21:25:27 +0300

    Testng added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,10 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
 
 class CashbackHackServiceTest {
 
@@ -11,12 +12,60 @@ class CashbackHackServiceTest {
 
     public void shouldCashbackHack() {
        CashbackHackService service = new CashbackHackService();
-       int amount = 1000;
+       int amount = 400;
 
-       int expected = 0;
+       int expected = 600;
        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+       
+    }
 
-        Assertions.assertEquals(expected, actual);
+    @Test
+
+    public void shouldCashbackHackZero() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+
+    public void shouldCashbackHackOne() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+
+    public void shouldCashbackHackBeforeMax() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+
+    public void shouldCashbackHackMax() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
     }
 
 }
